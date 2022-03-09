9060f398b5ae2b2cd01ab4043400faf9aaa8cc0f
Brian Cao <chasoft@users.noreply.github.com>
2022-03-09 08:13:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Remix Wiki
+
+The largest and most completed resource to learn about web fundamentals and Remix web framework
---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix Wiki</h1>
+      <p>The largest and most completed resource to learn about web fundamentals and Remix web framework</p>
+      <p>The project would be started soon... this is only the placeholder of this newly registered domain.</p>
+      <hr />
+      <p>A project by Chasoft Labs (chasoft.net) - We bet on Remix web framework</p>
+    </div>
+  );
+}
